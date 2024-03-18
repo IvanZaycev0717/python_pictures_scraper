9bbaa5738f358633ff181eaee3e1fc4e2e278ecd
Ivan Zaytsev <ivzaycev0717@yandex.ru>
2024-03-18 10:31:40 +0300

    parser_begin

--- picture_links_parser.py
+++ picture_links_parser.py
@@ -1,3 +1,21 @@
-class PictureLinksParcer:
-    def __init__(self):
-        pass
+import aiohttp
+import asyncio
+from pprint import pprint
+
+from bs4 import BeautifulSoup
+
+url = rf'https://www.flickr.com/search/?text=kittens'
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            html = await response.text()
+    soup = BeautifulSoup(html, 'lxml')
+    box = soup.find_all('div', class_='photo-list-photo-container')
+    for tag in box:
+        img_tag = tag.find('img')
+        src_value = img_tag.get('src')
+        print(src_value)
+
+asyncio.run(main())
